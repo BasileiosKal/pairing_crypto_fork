1edced9ba0e05ab78193f1c1ff7543a9fb3f075f
Vasilis Kalos <kalosbasileios@gmail.com>
2024-03-04 13:33:03 +0200

    refactor: rm deprecated detox runner

--- wrappers/react-native/example/__tests__/detox-environment.js
+++ wrappers/react-native/example/__tests__/detox-environment.js
@@ -1,20 +1,9 @@
-const { DetoxCircusEnvironment } = require('detox/runners/jest-circus');
+const { DetoxCircusEnvironment } = require('detox/runners/jest');
 const { FixturesEnvName, readFixtureFiles } = require('../__fixtures__/fixture-loader');
 
 class CustomDetoxEnvironment extends DetoxCircusEnvironment {
   constructor(config, context) {
     super(config, context);
-
-    // Can be safely removed, if you are content with the default value (=300000ms)
-    // this.initTimeout = 300000;
-    // this.initTimeout = 900000;
-
-    // This takes care of generating status logs on a per-spec basis. By default, Jest only reports at file-level.
-    // This is strictly optional.
-    // this.registerListeners({
-    //   SpecReporter,
-    //   WorkerAssignReporter,
-    // });
   }
 
   async setup() {
